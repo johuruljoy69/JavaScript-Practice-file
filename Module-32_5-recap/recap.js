06488cecc484311bfe34097a2e0aea0e6887e1ae
johirjoy <joy.sd69@gmail.com>
2023-02-23 02:21:05 +0600

    first commit

---
+++ Module-32_5-recap/recap.js
@@ -0,0 +1,32 @@
+/*
+Must Know:
+1. var, let, const
+2. default parameter
+3. template string
+3.1 create multiple string (``)
+3.2 and dynamic variable(expression): `${}`
+4. arrow function
+4.1 single parameter arrow function
+4.2 two parameter arrow function
+4.3 arrow function with function
+5. map. filter, find
+6. optional chain
+*/
+// Good to have
+/*
+1. spread operator [...number, 12, 34, 45]
+2. destructing
+2.1 object destructing: how to get property name vs variable const {x} = {x:3, y:4}
+2.2 array destructing: const [a,b] = [3,8,12,56]
+3. default parameter
+4. forEach 
+5. dot notation vs bracket notation
+*/
+
+// should know the name
+/*
+1. reduce
+2. inheritance
+3. prototypical chain 
+4. class, object
+*/
